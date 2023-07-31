8ef8c8aa772b375f742bf225aafd7990a37ab2a8
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-07-31 18:30:57 +0200

    Create userHandler.js

---
+++ src/handlers/userHandler.js
@@ -0,0 +1,111 @@
+const User = require("../models/userModel.js");
+const {
+  genPassword,
+  validatePassword,
+  issueJWT,
+} = require("../helpers/utils.js");
+
+const register = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const { hashedPassword, salt } = genPassword(password);
+
+    const user = await User.create({
+      username,
+      email,
+      authentication: { hashedPassword, salt },
+    });
+
+    res.status(201).json({
+      success: true,
+      user: {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({ success: false, error: err.message });
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).exec();
+
+    if (!user) {
+      return res
+        .status(401)
+        .json({ success: false, error: "Invalid credentials" });
+    }
+
+    const isValid = await validatePassword(
+      password,
+      user.authentication.hashedPassword,
+      user.authentication.salt
+    );
+
+    if (!isValid) {
+      return res
+        .status(401)
+        .json({ success: false, error: "Invalid credentials" });
+    }
+
+    const { token, expires } = issueJWT(user);
+
+    res.cookie("jwt", token, {
+      expires,
+      httpOnly: false,
+      secure: false,
+    });
+
+    return res.status(200).json({
+      success: true,
+      user: {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+      },
+    });
+  } catch (err) {
+    return res.status(400).json({ error: err.message });
+  }
+};
+
+const logout = (req, res) => {
+  if (req.cookies["jwt"]) {
+    res.clearCookie("jwt").status(200).json({
+      message: "You have logged out successfully.",
+    });
+  } else {
+    res.status(401).json({
+      success: false,
+      error: "Already logged out.",
+    });
+  }
+};
+
+const deleteAccount = async (req, res) => {
+  try {
+    const { id } = req.user;
+
+    await User.findByIdAndDelete(id).exec();
+
+    res.clearCookie("jwt");
+    res
+      .status(200)
+      .json({ success: true, message: "Account deleted successfully." });
+  } catch (err) {
+    res.status(500).json({ success: false, error: err.message });
+  }
+};
+
+module.exports = {
+  register,
+  login,
+  logout,
+  deleteAccount,
+};
