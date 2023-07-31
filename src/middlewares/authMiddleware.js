a4ff4b825670d79b55086ca9bb92391785c394fb
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-07-31 18:30:53 +0200

    Create authMiddleware.js

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,15 @@
+
+const passport = require("passport");
+
+function isAuthenticated(req, res, next) {
+  passport.authenticate("jwt", { session: false }, (err, user, info) => {
+    if (err || !user) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+    req.user = user;
+    
+    return next();
+  })(req, res, next);
+}
+
+module.exports = { isAuthenticated };
