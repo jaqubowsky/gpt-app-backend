eda62dd47e2acfd6489a35d412167f1ff6845234
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-08-03 02:35:03 +0200

    Create roomRoute.js

---
+++ src/routes/roomRoute.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const roomController = require("../controlers/roomController");
+
+(async () => {
+  const controller = await roomController();
+
+  router.post("/create", controller.create);
+  router.get("/:id", controller.getMessages);
+  router.post("/add/:id", controller.addUser);
+})();
+
+module.exports = router;
