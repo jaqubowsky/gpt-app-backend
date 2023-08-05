fecedd31f4fb79c9283acff0079ebf572f0c16b7
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-08-05 13:40:19 +0200

    Fix auth

--- src/routes/apiRoute.js
+++ src/routes/apiRoute.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const gptRoute = require("./gptRoute");
 const userRoute = require("./userRoute");
+const messageRoute = require("./messageRoute");
+const roomRoute = require("./roomRoute");
+const { isAuthenticated } = require("../middlewares/authMiddleware");
 
 const apiRouter = express.Router();
 
 apiRouter.use("/user", userRoute);
-apiRouter.use("/gpt", gptRoute);
+apiRouter.use("/message", isAuthenticated, messageRoute);
+apiRouter.use("/room", isAuthenticated, roomRoute);
+apiRouter.use("/gpt", isAuthenticated, gptRoute);
 
 module.exports = apiRouter;
--- src/routes/gptRoute.js
+++ src/routes/gptRoute.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { isAuthenticated } = require("../middlewares/authMiddleware");
 const gptController = require("../controlers/gptController");
 
 (async () => {
   const controller = await gptController();
 
-  router.post("/image", isAuthenticated, controller.image);
+  router.post("/image", controller.image);
 })();
 
 module.exports = router;
