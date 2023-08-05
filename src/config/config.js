6c9cb3fabc1993b8f3824f2869ac591bf9429e9e
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-08-05 13:40:41 +0200

    Add pusher vars

--- src/config/config.js
+++ src/config/config.js
@@ -9,6 +9,9 @@ const ALGORITHM = process.env.ALGORITHM;
 const PORT = process.env.PORT;
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 const OPENAI_ORGANIZATION_ID = process.env.OPENAI_ORGANIZATION_ID;
+const PUSHER_APP_ID = process.env.PUSHER_APP_ID;
+const PUSHER_KEY = process.env.PUSHER_KEY;
+const PUSHER_SECRET = process.env.PUSHER_SECRET;
 
 module.exports = {
   JWT_SECRET,
@@ -18,4 +21,7 @@ module.exports = {
   PORT,
   OPENAI_API_KEY,
   OPENAI_ORGANIZATION_ID,
+  PUSHER_APP_ID,
+  PUSHER_KEY,
+  PUSHER_SECRET,
 };
