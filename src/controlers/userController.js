8a70d19ee38fb3bc208b17366dde37660c4fc4d9
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-08-05 13:40:57 +0200

    add getMe and getRooms

--- src/controlers/userController.js
+++ src/controlers/userController.js
@@ -3,10 +3,14 @@ const {
   register,
   deleteAccount,
   logout,
+  getMe,
+  getUserRooms,
 } = require("../handlers/userHandler.js");
 
 const userController = async () => {
   return {
+    getMe: await getMe,
+    getRooms: await getUserRooms,
     signup: await register,
     login: await login,
     logout: await logout,
